f605a79ab84106e98a21038eaffbc27a62065679
Fred <frederic.brossard.pf1@gmail.com>
2018-05-22 10:59:15 +0200

    Commit initial SpringBoot

---
+++ src/main/java/com/example/demo/Session4ProjetSpringboot2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session4ProjetSpringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Session4ProjetSpringboot2Application.class, args);
+	}
+}
